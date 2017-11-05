61334804715bd60aa39a776284f3e4eef89c22d0
linshuizhaoying <4799109@qq.com>
2017-11-05 16:19:03 +0800

    webhook

--- deploy/index.js
+++ deploy/index.js
@@ -1,5 +1,27 @@
 var spawn = require('child_process').spawn
+var http = require('http')
+var spawn = require('child_process').spawn
+var createHandler = require('github-Webhooks-handler')
+var handler = createHandler({ path: '/pushCode', secret: '' })
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404;
+    res.end('no such location')
+  })
+}).listen(7777)
 
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+// 监听 push 事件
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+    init()
+}
+)
 function rumCommand( cmd, args, cwd, callback ) {
   var child = spawn( cmd, args, {cwd: cwd} )
   var response = ''
