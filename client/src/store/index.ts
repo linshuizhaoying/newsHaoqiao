322ad7aad636c063d33863e14124288eb5faf54a
linshuizhaoying <4799109@qq.com>
2017-12-05 18:11:28 +0800

    fix windows suit

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,16 +1,17 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import reducers from '../reducers'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 import { errorReporter } from '../middleware/errorHandle';
 
 export function configureStore (preloadState: any = {}) {
   const store = createStore(
     reducers,
     preloadState,
-    compose(
-      applyMiddleware(thunk, logger, errorReporter),
-      (window as any).devToolsExtension && (window as any).devToolsExtension()
+    composeWithDevTools(
+      applyMiddleware(thunk, logger, errorReporter)
+     // (window as any).devToolsExtension && (window as any).devToolsExtension()
     )
   )
 
